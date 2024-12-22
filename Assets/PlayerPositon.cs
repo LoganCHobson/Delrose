facd759f796242e59f4c9a0945ae494c43a74ecd
Logan <Loganhobson31@gmail.com>
2024-12-21 18:47:35 -0600

    Idk man. This should be working.

--- Assets/PlayerPositon.cs
+++ Assets/PlayerPositon.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
 
 public class PlayerPosition : NetworkBehaviour
 {
     private Vector3 lastKnownPosition;
-    private Dictionary<ulong, Vector3> allPlayerPositions = new Dictionary<ulong, Vector3>();
+    private Positions allPlayerPositions;
 
     #region SoloRequest
 
@@ -56,13 +59,29 @@ public class PlayerPosition : NetworkBehaviour
             }
         }
 
+        List<ulong> clientsList = new List<ulong>();
+        List<Vector3> positionList = new List<Vector3>();
+
+        
+        foreach (var pos in clientPositions)
+        {
+            clientsList.Add(pos.Key);
+            positionList.Add(pos.Value);
+        }
         
 
-        SendAllPositionClientRpc(clientPositions);
+        Positions positions = new Positions
+        {
+            clientId = clientsList.ToArray(),
+            position = positionList.ToArray(),
+        };
+
+
+        SendAllPositionClientRpc(positions);
     }
 
     [ClientRpc]
-    private void SendAllPositionClientRpc(Dictionary<ulong, Vector3> _positions)
+    private void SendAllPositionClientRpc(Positions _positions)
     {
         if (IsOwner)
         {
@@ -80,10 +99,32 @@ public class PlayerPosition : NetworkBehaviour
 
     public Dictionary<ulong, Vector3> GetAllPlayerPositions()
     {
-        return allPlayerPositions;
+        Dictionary<ulong, Vector3> playerPositions = new Dictionary<ulong, Vector3>();
+
+        if (allPlayerPositions != null)
+        {
+            for (int i = 0; i < allPlayerPositions.clientId.Length; i++)
+            {
+                playerPositions[allPlayerPositions.clientId[i]] = allPlayerPositions.position[i];
+            }
+        }
+
+        return playerPositions;
     }
 
     #endregion
 }
 
+public class Positions : INetworkSerializable
+{
+    public ulong[] clientId;
+    public Vector3[] position;
+   
 
+    void INetworkSerializable.NetworkSerialize<T>(BufferSerializer<T> serializer)
+    {
+        serializer.SerializeValue(ref clientId);
+        serializer.SerializeValue(ref position);
+
+    }
+}
